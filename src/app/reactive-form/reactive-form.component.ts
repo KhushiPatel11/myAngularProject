161338542178d16ae911c413b9005c50cccdc858
Khushi Patel <khushi@EVISION.COM>
2025-03-19 19:03:06 +0530

    Your commit message

--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { customValidators } from '../Validators/noSpaceAllowed.validator'
 
 @Component({
   selector: 'app-reactive-form',
@@ -12,8 +13,8 @@ export class ReactiveFormComponent implements OnInit{
   
   ngOnInit(): void {
     this.reactiveForm = new FormGroup({
-      firstname: new FormControl(null, Validators.required),
-      lastname: new FormControl(null, Validators.required),
+      firstname: new FormControl('', [Validators.required, customValidators.noSpaceAllowed]),
+      lastname: new FormControl('', [Validators.required, customValidators.noSpaceAllowed]),
       email: new FormControl(null, [Validators.required, Validators.email]),
       dob: new FormControl(null),
       gender: new FormControl(null),
@@ -26,7 +27,14 @@ export class ReactiveFormComponent implements OnInit{
       }),
       skills: new FormArray([
         new FormControl(null, Validators.required)
-      ]) 
+      ]),
+     experience: new FormArray([
+      
+     ])
+    })
+
+    this.reactiveForm.get('firstname').valueChanges.subscribe((value) => {
+      console.log(value);
     })
   }
 
@@ -40,7 +48,23 @@ export class ReactiveFormComponent implements OnInit{
   }
 
   DeleteSkills(index: number){
-    const control =  <FormArray>this.reactiveForm.get('skills')
+    const control =  <FormArray>this.reactiveForm.get('skills');
     control.removeAt(index);
+  }
+
+  AddExperience(){
+    const frmGrp =   new FormGroup({
+      company: new FormControl(null),
+      position: new FormControl(null),
+      totalExp: new FormControl(null),
+      start: new FormControl(null),
+      end: new FormControl(null),
+    });
+    (<FormArray>this.reactiveForm.get('experience')).push(frmGrp);
+  }
+
+  DeleteExperience(index: number){
+    const frmArray = <FormArray>this.reactiveForm.get('experience');
+    frmArray.removeAt(index);
   }
 }
